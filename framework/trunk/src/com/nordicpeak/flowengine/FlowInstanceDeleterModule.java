c86d6bb896c8d416afa3d08ab2c7488598c57628
robertolofsson <robertolofsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2017-02-27 08:31:27 +0000

    - Added new methods to FlowInstanceDeleterModule
    git-svn-id: svn://svn.sundsvall.se/oep-dev@170 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- framework/trunk/src/com/nordicpeak/flowengine/FlowInstanceDeleterModule.java
+++ framework/trunk/src/com/nordicpeak/flowengine/FlowInstanceDeleterModule.java
@@ -15,11 +15,13 @@ import se.unlogic.hierarchy.core.events.CRUDEvent;
 import se.unlogic.hierarchy.core.interfaces.ForegroundModuleResponse;
 import se.unlogic.hierarchy.foregroundmodules.AnnotatedForegroundModule;
 import se.unlogic.standardutils.dao.HighLevelQuery;
+import se.unlogic.standardutils.dao.LowLevelQuery;
 import se.unlogic.standardutils.dao.QueryOperators;
 import se.unlogic.standardutils.dao.QueryParameterFactory;
 import se.unlogic.standardutils.dao.TransactionHandler;
 import se.unlogic.standardutils.numbers.NumberUtils;
 import se.unlogic.standardutils.random.RandomUtils;
+import se.unlogic.standardutils.string.StringUtils;
 import se.unlogic.webutils.http.URIParser;
 
 import com.nordicpeak.flowengine.beans.FlowInstance;
@@ -41,7 +43,9 @@ public class FlowInstanceDeleterModule extends AnnotatedForegroundModule {
 	
 	private int randomNumber = RandomUtils.getRandomInt(10000, 10000000);
 	
-	private int randomInstanceNumber = RandomUtils.getRandomInt(10000, 10000000);
+	private int randomSelectedNumber = RandomUtils.getRandomInt(10000, 10000000);
+	
+	private int randomFlowNumber = RandomUtils.getRandomInt(10000, 10000000);
 
 	@Override
 	protected void createDAOs(DataSource dataSource) throws Exception {
@@ -99,7 +103,7 @@ public class FlowInstanceDeleterModule extends AnnotatedForegroundModule {
 						
 						for(QueryInstanceDescriptor queryInstanceDescriptor : queryInstanceDescriptors){
 							
-							log.info("Deleting " + queryInstanceDescriptor);
+							log.info("Deleting query instance: " + queryInstanceDescriptor);
 							try {
 								this.queryHandler.deleteQueryInstance(queryInstanceDescriptor, transactionHandler);
 							} catch (Exception e) {
@@ -131,15 +135,15 @@ public class FlowInstanceDeleterModule extends AnnotatedForegroundModule {
 	}
 
 	@WebPublic
-	public synchronized ForegroundModuleResponse deleteFlowInstances(HttpServletRequest req, HttpServletResponse res, User user, URIParser uriParser) throws Throwable {
+	public synchronized ForegroundModuleResponse deleteSelectedInstances(HttpServletRequest req, HttpServletResponse res, User user, URIParser uriParser) throws Throwable {
 		
 		Integer submittedID = NumberUtils.toInt(req.getParameter("instance-code"));
 		
 		List<Integer> flowInstanceIDs = NumberUtils.toInt(req.getParameterValues("id"));
 		
-		if(submittedID == null || submittedID != randomInstanceNumber || flowInstanceIDs == null){
+		if(submittedID == null || submittedID != randomSelectedNumber || flowInstanceIDs == null){
 			
-			return new SimpleForegroundModuleResponse(getHTML("Flow instance deleter (use with caution!)","Submit the instance-code " + randomInstanceNumber + " and id parameters to delete selected flow instances."));
+			return new SimpleForegroundModuleResponse(getHTML("Flow instance deleter (use with caution!)","Submit the instance-code " + randomSelectedNumber + " and id parameters to delete selected flow instances."));
 		}
 	
 		int deletedCount = 0;
@@ -169,7 +173,7 @@ public class FlowInstanceDeleterModule extends AnnotatedForegroundModule {
 						
 						for(QueryInstanceDescriptor queryInstanceDescriptor : queryInstanceDescriptors){
 							
-							log.info("Deleting " + queryInstanceDescriptor);
+							log.info("Deleting query instance: " + queryInstanceDescriptor);
 							try {
 								this.queryHandler.deleteQueryInstance(queryInstanceDescriptor, transactionHandler);
 							} catch (Exception e) {
@@ -192,13 +196,83 @@ public class FlowInstanceDeleterModule extends AnnotatedForegroundModule {
 			
 		}finally{
 			
-			randomInstanceNumber = RandomUtils.getRandomInt(10000, 10000000);
+			randomSelectedNumber = RandomUtils.getRandomInt(10000, 10000000);
 			
 			TransactionHandler.autoClose(transactionHandler);
 		}
 		
 		return new SimpleForegroundModuleResponse(getHTML("Flow instances deleted",deletedCount + " flow instances deleted."));		
 	}
+	
+	@WebPublic
+	public synchronized ForegroundModuleResponse deleteFlowInstances(HttpServletRequest req, HttpServletResponse res, User user, URIParser uriParser) throws Throwable {
+		
+		Integer submittedID = NumberUtils.toInt(req.getParameter("flow-code"));
+		
+		List<Integer> flowIDs = NumberUtils.toInt(req.getParameterValues("id"));
+		
+		if(submittedID == null || submittedID != randomFlowNumber || flowIDs == null){
+			
+			return new SimpleForegroundModuleResponse(getHTML("Flow instance deleter (use with caution!)","Submit the flow-code " + randomFlowNumber + " and id parameters to delete selected all flow instances for the given flows"));
+		}
+	
+		int deletedCount = 0;
+		
+		log.info("User " + user + " deleting all flow instances with ID's: " + flowIDs);
+		
+		TransactionHandler transactionHandler = null;
+		
+		try{
+			transactionHandler = new TransactionHandler(dataSource);
+			
+			LowLevelQuery<FlowInstance> query = new LowLevelQuery<FlowInstance>("SELECT * FROM " + this.daoFactory.getFlowInstanceDAO().getTableName() + " WHERE flowID IN (" + (StringUtils.toCommaSeparatedString(flowIDs)) + ")");
+				
+			List<FlowInstance> flowInstances = this.daoFactory.getFlowInstanceDAO().getAll(query, transactionHandler);
+
+			if(flowInstances != null){
+				
+				for(FlowInstance flowInstance : flowInstances){
+					
+					HighLevelQuery<QueryInstanceDescriptor> descriptorQuery = new HighLevelQuery<QueryInstanceDescriptor>(queryInstanceDescriptorFlowInstanceIDParamFactory.getParameter(flowInstance.getFlowInstanceID()));
+					
+					descriptorQuery.addRelation(QueryInstanceDescriptor.QUERY_DESCRIPTOR_RELATION);
+					
+					List<QueryInstanceDescriptor> queryInstanceDescriptors = daoFactory.getQueryInstanceDescriptorDAO().getAll(descriptorQuery, transactionHandler);
+					
+					if(queryInstanceDescriptors != null){
+						
+						for(QueryInstanceDescriptor queryInstanceDescriptor : queryInstanceDescriptors){
+							
+							log.info("Deleting query instance: " + queryInstanceDescriptor);
+							try {
+								this.queryHandler.deleteQueryInstance(queryInstanceDescriptor, transactionHandler);
+							} catch (Exception e) {
+								log.warn("Error deleting query instance " + queryInstanceDescriptor);
+							}
+						}
+					}
+					
+					log.info("Deteing " + flowInstance);
+					daoFactory.getFlowInstanceDAO().delete(flowInstance, transactionHandler);
+				}
+			
+				deletedCount = flowInstances.size();
+				
+			}
+			
+			transactionHandler.commit();
+			
+			systemInterface.getEventHandler().sendEvent(FlowInstance.class, new CRUDEvent<FlowInstance>(FlowInstance.class, CRUDAction.DELETE, flowInstances), se.unlogic.hierarchy.core.enums.EventTarget.ALL);
+			
+		}finally{
+			
+			randomFlowNumber = RandomUtils.getRandomInt(10000, 10000000);
+			
+			TransactionHandler.autoClose(transactionHandler);
+		}
+		
+		return new SimpleForegroundModuleResponse(getHTML("Flow instances deleted",deletedCount + " flow instances deleted."));		
+	}	
 	
 	private String getHTML(String title, String message) {
 
