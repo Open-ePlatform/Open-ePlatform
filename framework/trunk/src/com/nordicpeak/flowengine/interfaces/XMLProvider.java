72a6966b236c0f6267d019c5684223b9209b8012
robertolofsson <robertolofsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2017-05-04 12:45:36 +0000

    - Updated interface with generateXML method
    git-svn-id: svn://svn.sundsvall.se/oep-dev@268 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- framework/trunk/src/com/nordicpeak/flowengine/interfaces/XMLProvider.java
+++ framework/trunk/src/com/nordicpeak/flowengine/interfaces/XMLProvider.java
@@ -1,10 +1,16 @@
 package com.nordicpeak.flowengine.interfaces;
 
 import java.io.File;
+import java.sql.Timestamp;
+
+import com.nordicpeak.flowengine.beans.FlowInstanceEvent;
+import com.nordicpeak.flowengine.managers.FlowInstanceManager;
 
 
 
 public interface XMLProvider {
 
 	public File getXML(Integer flowInstanceID, Integer eventID);
+	
+	public void generateXML(ImmutableFlowInstance flowInstance, FlowInstanceManager flowInstanceManager, FlowInstanceEvent event, Timestamp lastSubmitted, File outputFile) throws Exception;
 }
