1c7f7a4b96644df0afb1333c2d29e2a17175b105
antonolsson <antonolsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2017-10-03 10:54:55 +0000

    - New ContactDetailQuery
    git-svn-id: svn://svn.sundsvall.se/oep-dev@421 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- framework/trunk/src/com/nordicpeak/flowengine/beans/RequestMetadata.java
+++ framework/trunk/src/com/nordicpeak/flowengine/beans/RequestMetadata.java
@@ -3,11 +3,14 @@ package com.nordicpeak.flowengine.beans;
 import java.io.Serializable;
 
 import se.unlogic.standardutils.xml.GeneratedElementable;
+import se.unlogic.standardutils.xml.XMLElement;
 
+@XMLElement
 public class RequestMetadata extends GeneratedElementable implements Serializable {
 
 	private static final long serialVersionUID = -2715157789979993384L;
 
+	@XMLElement
 	private final boolean manager;
 
 	public RequestMetadata(boolean manager) {
