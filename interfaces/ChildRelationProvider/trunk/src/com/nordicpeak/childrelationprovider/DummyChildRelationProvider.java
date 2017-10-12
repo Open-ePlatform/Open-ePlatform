286acea1a38d6bca90203567e3dd2b8bc810e0aa
antonolsson <antonolsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2017-10-12 12:33:26 +0000

    - Add DummyChildRelationProvider
    git-svn-id: svn://svn.sundsvall.se/oep-dev@437 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

---
+++ interfaces/ChildRelationProvider/trunk/src/com/nordicpeak/childrelationprovider/DummyChildRelationProvider.java
@@ -0,0 +1,104 @@
+package com.nordicpeak.childrelationprovider;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import se.unlogic.hierarchy.core.annotations.CheckboxSettingDescriptor;
+import se.unlogic.hierarchy.core.annotations.ModuleSetting;
+import se.unlogic.hierarchy.core.annotations.TextFieldSettingDescriptor;
+import se.unlogic.hierarchy.core.interfaces.ForegroundModuleDescriptor;
+import se.unlogic.hierarchy.core.interfaces.SectionInterface;
+import se.unlogic.hierarchy.foregroundmodules.AnnotatedForegroundModule;
+import se.unlogic.standardutils.populators.SwedishSocialSecurity12DigitsWithoutMinusPopulator;
+
+import com.nordicpeak.childrelationprovider.exceptions.ChildRelationProviderException;
+
+public class DummyChildRelationProvider extends AnnotatedForegroundModule implements ChildRelationProvider {
+	
+	
+	@ModuleSetting
+	@TextFieldSettingDescriptor(name = "1 - citizen ID", description = "", required = true, formatValidator = SwedishSocialSecurity12DigitsWithoutMinusPopulator.class)
+	private String childID1 = "201001011236";
+	
+	@ModuleSetting
+	@TextFieldSettingDescriptor(name = "2 - citizen ID", description = "", required = true, formatValidator = SwedishSocialSecurity12DigitsWithoutMinusPopulator.class)
+	private String childID2 = "200101011229";
+	
+	@ModuleSetting
+	@CheckboxSettingDescriptor(name = "1 - enable", description = "")
+	private boolean child1;
+	
+	@ModuleSetting
+	@CheckboxSettingDescriptor(name = "2 - enable", description = "")
+	private boolean child2;
+	
+	@ModuleSetting
+	@CheckboxSettingDescriptor(name = "1 - secret", description = "")
+	private boolean secret1;
+	
+	@ModuleSetting
+	@CheckboxSettingDescriptor(name = "2 - secret", description = "")
+	private boolean secret2;
+	
+	@ModuleSetting
+	@CheckboxSettingDescriptor(name = "1 - ensam vårdnad", description = "")
+	private boolean ensam1;
+	
+	@Override
+	public void init(ForegroundModuleDescriptor moduleDescriptor, SectionInterface sectionInterface, DataSource dataSource) throws Exception {
+		
+		super.init(moduleDescriptor, sectionInterface, dataSource);
+		
+		if (!systemInterface.getInstanceHandler().addInstance(ChildRelationProvider.class, this)) {
+			
+			throw new RuntimeException("Unable to register module " + moduleDescriptor + " in global instance handler using key " + ChildRelationProvider.class.getSimpleName() + ", another instance is already registered using this key.");
+		}
+	}
+	
+	@Override
+	public void unload() throws Exception {
+		
+		systemInterface.getInstanceHandler().removeInstance(ChildRelationProvider.class, this);
+		
+		super.unload();
+	}
+	
+	@Override
+	public Map<String, Child> getChildrenWithGuardians(String citizenIdentifier) throws ChildRelationProviderException {
+		
+		Map<String, Child> children = new HashMap<String, Child>();
+		
+		if (child1) {
+			
+			SimpleChild child1 = new SimpleChild("kalle", "kula", childID1);
+			
+			if (ensam1) {
+				
+				child1.setGuardians(Arrays.asList(new Guardian[] { new SimpleGuardian("förälder", "1", citizenIdentifier) }));
+				
+			} else {
+				
+				child1.setGuardians(Arrays.asList(new Guardian[] { new SimpleGuardian("förälder", "1", citizenIdentifier), new SimpleGuardian("förälder", "2", "191234567890") }));
+			}
+			
+			child1.setSecrecy(secret1);
+			
+			children.put(child1.getCitizenIdentifier(), child1);
+		}
+		
+		if (child2) {
+			
+			SimpleChild child2 = new SimpleChild("lisa", "kula", childID2);
+			child2.setGuardians(Arrays.asList(new Guardian[] { new SimpleGuardian("förälder", "1", citizenIdentifier), new SimpleGuardian("förälder", "2", "191234567890") }));
+			child2.setSecrecy(secret2);
+			
+			children.put(child2.getCitizenIdentifier(), child2);
+		}
+		
+		return children;
+	}
+	
+}
