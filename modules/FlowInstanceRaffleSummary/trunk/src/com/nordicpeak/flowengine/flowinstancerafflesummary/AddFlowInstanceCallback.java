df3f1544de6d7422d6c053aa93fc1533959ee1aa
antonolsson <antonolsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2017-11-16 15:32:43 +0000

    - Update for changes to AddFlowInstanceModule
    git-svn-id: svn://svn.sundsvall.se/oep-dev@489 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- modules/FlowInstanceRaffleSummary/trunk/src/com/nordicpeak/flowengine/flowinstancerafflesummary/AddFlowInstanceCallback.java
+++ modules/FlowInstanceRaffleSummary/trunk/src/com/nordicpeak/flowengine/flowinstancerafflesummary/AddFlowInstanceCallback.java
@@ -2,7 +2,10 @@ package com.nordicpeak.flowengine.flowinstancerafflesummary;
 
 import java.io.Serializable;
 
+import javax.servlet.http.HttpSession;
+
 import com.nordicpeak.flowengine.beans.FlowInstance;
+import com.nordicpeak.flowengine.managers.FlowInstanceManager;
 import com.nordicpeak.flowengine.managers.MutableFlowInstanceManager;
 
 public class AddFlowInstanceCallback implements com.nordicpeak.flowengine.addflowinstance.AddFlowInstanceCallback, Serializable {
@@ -10,10 +13,19 @@ public class AddFlowInstanceCallback implements com.nordicpeak.flowengine.addflo
 	private static final long serialVersionUID = -1518909826970607855L;
 
 	@Override
-	public void addFlowInstanceBeforeSave(MutableFlowInstanceManager instanceManager) {
+	public void beforeSave(MutableFlowInstanceManager instanceManager) {
 		
 		FlowInstance flowInstance = (FlowInstance) instanceManager.getFlowInstance();
 		flowInstance.getAttributeHandler().setAttribute("manual", "true");
+	}
+
+	@Override
+	public String getReturnURL(FlowInstanceManager instanceManager) {
+		return null;
+	}
+
+	@Override
+	public void afterSave(FlowInstanceManager instanceManager, HttpSession session) {
 	}
 	
 }
