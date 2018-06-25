b590be1135abfccec068045dbf80904d6179ab6f
antonolsson <antonolsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2018-06-25 13:06:03 +0000

    - Remove TODO
    git-svn-id: svn://svn.sundsvall.se/oep-dev@849 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- framework/branches/restrictedmanagers/src/com/nordicpeak/flowengine/utils/FlowInstanceUtils.java
+++ framework/branches/restrictedmanagers/src/com/nordicpeak/flowengine/utils/FlowInstanceUtils.java
@@ -48,7 +48,6 @@ public class FlowInstanceUtils {
 			builder.append(" (");
 		}
 		
-		//TODO order does not match StandardFlowNotificationHandler.getPosterContact
 		ImmutableFlowInstanceEvent submitEvent = getLatestSubmitEvent(flowInstance);
 		
 		if (submitEvent != null && submitEvent.getPoster() != null) {
