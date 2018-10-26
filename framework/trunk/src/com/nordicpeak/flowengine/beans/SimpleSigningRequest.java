d2e06ee1606278c6529b2b3de1820030bf2ee6d6
robertolofsson <robertolofsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2018-10-26 12:13:01 +0000

    - Added toString
    git-svn-id: svn://svn.sundsvall.se/oep-dev@1003 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- framework/trunk/src/com/nordicpeak/flowengine/beans/SimpleSigningRequest.java
+++ framework/trunk/src/com/nordicpeak/flowengine/beans/SimpleSigningRequest.java
@@ -2,42 +2,54 @@ package com.nordicpeak.flowengine.beans;
 
 import javax.servlet.http.HttpServletRequest;
 
+import se.unlogic.standardutils.string.StringUtils;
+
 import com.nordicpeak.flowengine.interfaces.GenericSigningRequest;
 
 public class SimpleSigningRequest implements GenericSigningRequest {
-	
+
 	private final String description;
 	private final String dataToSign;
 	private final String signingFormURL;
 	private final String processSigningURL;
-	
+
 	public SimpleSigningRequest(String description, String dataToSign, String signingFormURL, String processSigningURL) {
+
 		super();
-		
+
 		this.description = description;
 		this.dataToSign = dataToSign;
 		this.signingFormURL = signingFormURL;
 		this.processSigningURL = processSigningURL;
 	}
-	
+
 	@Override
 	public String getDescription() {
+
 		return description;
 	}
-	
+
 	@Override
 	public String getDataToSign() {
+
 		return dataToSign;
 	}
 
 	@Override
 	public String getSigningFormURL(HttpServletRequest req) {
+
 		return signingFormURL;
 	}
 
 	@Override
 	public String getProcessSigningURL(HttpServletRequest req) {
+
 		return processSigningURL;
 	}
-	
+
+	@Override
+	public String toString() {
+
+		return StringUtils.toLogFormat(description, 40);
+	}
 }
