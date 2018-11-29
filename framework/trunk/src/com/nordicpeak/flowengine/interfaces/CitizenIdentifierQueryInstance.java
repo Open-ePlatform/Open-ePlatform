994b1464ac6a1231c3c26f622536195976a5338b
antonolsson <antonolsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2018-11-29 16:31:23 +0000

    - Add isTestCitizenIdentifier to CitizenIdentifierQueryInstance interface
    git-svn-id: svn://svn.sundsvall.se/oep-dev@1029 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- framework/trunk/src/com/nordicpeak/flowengine/interfaces/CitizenIdentifierQueryInstance.java
+++ framework/trunk/src/com/nordicpeak/flowengine/interfaces/CitizenIdentifierQueryInstance.java
@@ -3,5 +3,7 @@ package com.nordicpeak.flowengine.interfaces;
 public interface CitizenIdentifierQueryInstance extends QueryInstance {
 
 	String getCitizenIdentifier();
+	
+	boolean isTestCitizenIdentifier();
 
 }
