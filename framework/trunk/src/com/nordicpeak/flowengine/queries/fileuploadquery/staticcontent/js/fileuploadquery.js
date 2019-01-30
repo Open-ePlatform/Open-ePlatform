8b0b55031db02fbf6b3d7a856f5539fb2971823d
martinkero <martinkero@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2019-01-30 11:39:59 +0000

    - FileUploadQuery: Added drag and drop, disabled in IE/Edge due to lack of ability to modify input files property
    git-svn-id: svn://svn.sundsvall.se/oep-dev@1067 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- framework/trunk/src/com/nordicpeak/flowengine/queries/fileuploadquery/staticcontent/js/fileuploadquery.js
+++ framework/trunk/src/com/nordicpeak/flowengine/queries/fileuploadquery/staticcontent/js/fileuploadquery.js
@@ -1,6 +1,48 @@
 $(document).ready(function() {
 	
 	setQueryRequiredFunctions["FileUploadQueryInstance"] = makeFileUploatQueryRequired;
+	
+	var ua = window.navigator.userAgent;
+	var isMSBrowser = ua.indexOf('MSIE ') > -1 || ua.indexOf('Trident/') > -1 || ua.indexOf('Edge/') > -1;
+	
+	if (isMSBrowser) {
+
+		$('.upload')
+			.addClass('no-hover')
+			.on('drop dragenter dragover', function(e){ 
+			
+				e.preventDefault();
+		});
+		
+	} else {
+		
+		$('.upload').on('drop', function(e){
+			
+			e.preventDefault();
+			$(this).removeClass('active');
+			
+			var $input = $(this).find('input:last');
+			var droppedFiles = e.originalEvent.dataTransfer.files;
+			
+			$input.prop('files', droppedFiles);
+
+			// Some browsers triggers 'change' on property change, if not we trigger it manually
+			if ($input.is(':last-child')) {
+				
+				$input.trigger('change');
+			}
+			
+		}).on('dragenter dragover', function(e){
+			
+			e.preventDefault();
+			$(this).addClass('active');
+			
+		}).on('dragleave', function(e){
+			
+			$(this).removeClass('active');
+		});
+	}
+	
 });
 
 function makeFileUploatQueryRequired(queryID) {
