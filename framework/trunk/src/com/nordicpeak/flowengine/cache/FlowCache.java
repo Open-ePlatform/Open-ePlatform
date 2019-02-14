e68f3242027e7ec36b85b74e88713434dd785a56
antonolsson <antonolsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2019-02-14 16:20:04 +0000

    - Add FlowCacheAccess
    git-svn-id: svn://svn.sundsvall.se/oep-dev@1095 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- framework/trunk/src/com/nordicpeak/flowengine/cache/FlowCache.java
+++ framework/trunk/src/com/nordicpeak/flowengine/cache/FlowCache.java
@@ -1,10 +1,16 @@
 package com.nordicpeak.flowengine.cache;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
+
+import se.unlogic.hierarchy.core.beans.User;
+import se.unlogic.standardutils.collections.CollectionUtils;
 
 import com.nordicpeak.flowengine.beans.Flow;
 import com.nordicpeak.flowengine.beans.FlowFamily;
+import com.nordicpeak.flowengine.enums.ManagerAccess;
 
 public class FlowCache {
 
@@ -26,5 +32,30 @@ public class FlowCache {
 	public HashMap<Integer, FlowFamily> getFlowFamilyCacheMap() {
 
 		return flowFamilyCacheMap;
+	}
+	
+	public FlowCacheAccess getManagedFlowFamilies(User user) {
+		
+		List<FlowFamily> flowFamiliesFullAccess = new ArrayList<FlowFamily>();
+		List<FlowFamily> flowFamiliesRestrictedAccess = null;
+		
+		for (FlowFamily flowFamily : flowFamilyCacheMap.values()) {
+			
+			ManagerAccess access = flowFamily.getManagerAccess(user);
+			
+			if (access == ManagerAccess.FULL) {
+				flowFamiliesFullAccess.add(flowFamily);
+				
+			} else if (access == ManagerAccess.RESTRICTED) {
+				
+				flowFamiliesRestrictedAccess = CollectionUtils.addAndInstantiateIfNeeded(flowFamiliesRestrictedAccess, flowFamily);
+			}
+		}
+		
+		if (flowFamiliesFullAccess.isEmpty()) {
+			flowFamiliesFullAccess = null;
+		}
+		
+		return new FlowCacheAccess(flowFamiliesFullAccess, flowFamiliesRestrictedAccess);
 	}
 }
