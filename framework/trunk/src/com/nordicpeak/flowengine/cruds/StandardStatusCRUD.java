3e782ff71c1405fa64ec181f67eae0caa463f65b
robertolofsson <robertolofsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2019-08-30 13:37:28 +0000

    - Fixed Eclipse warnings
    git-svn-id: svn://svn.sundsvall.se/oep-dev@1331 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- framework/trunk/src/com/nordicpeak/flowengine/cruds/StandardStatusCRUD.java
+++ framework/trunk/src/com/nordicpeak/flowengine/cruds/StandardStatusCRUD.java
@@ -4,7 +4,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
