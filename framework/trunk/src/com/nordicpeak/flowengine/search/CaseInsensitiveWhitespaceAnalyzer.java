bc8e3a2b9d6e33905c8ffbed7fec9c0cdbd8ce76
robertolofsson <robertolofsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2021-02-11 16:02:56 +0000

    - Restored old analyser
    git-svn-id: svn://svn.sundsvall.se/oep-dev@1819 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

---
+++ framework/trunk/src/com/nordicpeak/flowengine/search/CaseInsensitiveWhitespaceAnalyzer.java
@@ -0,0 +1,19 @@
+package com.nordicpeak.flowengine.search;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.core.LowerCaseFilter;
+import org.apache.lucene.analysis.core.WhitespaceTokenizer;
+
+
+public class CaseInsensitiveWhitespaceAnalyzer extends Analyzer {
+    
+	@Override
+	protected TokenStreamComponents createComponents(String fieldName) {
+
+		Tokenizer tokenizer = new WhitespaceTokenizer();
+        TokenStream filter = new LowerCaseFilter(tokenizer);
+        return new TokenStreamComponents(tokenizer, filter);
+	}	
+}
