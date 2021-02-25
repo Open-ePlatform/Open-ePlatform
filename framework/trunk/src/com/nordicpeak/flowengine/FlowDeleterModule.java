bf4a8d007cf1eea4fbce2c5ef7edc95de6c4e819
robertolofsson <robertolofsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2021-02-25 15:42:37 +0000

    - Added new flow delete method
    git-svn-id: svn://svn.sundsvall.se/oep-dev@1837 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- framework/trunk/src/com/nordicpeak/flowengine/FlowDeleterModule.java
+++ framework/trunk/src/com/nordicpeak/flowengine/FlowDeleterModule.java
@@ -1,5 +1,6 @@
 package com.nordicpeak.flowengine;
 
+import java.lang.reflect.Field;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 import se.unlogic.hierarchy.core.annotations.InstanceManagerDependency;
+import se.unlogic.hierarchy.core.annotations.WebPublic;
 import se.unlogic.hierarchy.core.beans.SimpleForegroundModuleResponse;
 import se.unlogic.hierarchy.core.beans.User;
 import se.unlogic.hierarchy.core.enums.CRUDAction;
@@ -36,7 +38,7 @@ import com.nordicpeak.flowengine.interfaces.QueryHandler;
 
 public class FlowDeleterModule extends AnnotatedForegroundModule {
 
-	private static final HighLevelQuery<Flow> FLOW_QUERY = new HighLevelQuery<Flow>(Flow.STEPS_RELATION, Flow.FLOW_FAMILY_RELATION, Step.QUERY_DESCRIPTORS_RELATION, QueryDescriptor.EVALUATOR_DESCRIPTORS_RELATION);
+	private static final List<Field> FLOW_RELATIONS = CollectionUtils.getList(Flow.STEPS_RELATION, Flow.FLOW_FAMILY_RELATION, Step.QUERY_DESCRIPTORS_RELATION, QueryDescriptor.EVALUATOR_DESCRIPTORS_RELATION);
 
 	@InstanceManagerDependency(required = true)
 	protected QueryHandler queryHandler;
@@ -47,8 +49,9 @@ public class FlowDeleterModule extends AnnotatedForegroundModule {
 	protected FlowEngineDAOFactory daoFactory;
 
 	protected QueryParameterFactory<Flow, FlowFamily> flowFlowFamilyParamFactory;
+	protected QueryParameterFactory<Flow, Integer> flowIDParamFactory;
 	protected QueryParameterFactory<FlowInstance, Flow> flowInstanceFlowParamFactory;
-
+	
 	private int randomNumber = RandomUtils.getRandomInt(10000, 10000000);
 
 	@Override
@@ -57,6 +60,7 @@ public class FlowDeleterModule extends AnnotatedForegroundModule {
 		this.daoFactory = new FlowEngineDAOFactory(dataSource, systemInterface.getUserHandler(), systemInterface.getGroupHandler());
 
 		flowFlowFamilyParamFactory = daoFactory.getFlowDAO().getParamFactory("flowFamily", FlowFamily.class);
+		flowIDParamFactory = daoFactory.getFlowDAO().getParamFactory("flowID", Integer.class);
 		flowInstanceFlowParamFactory = daoFactory.getFlowInstanceDAO().getParamFactory("flow", Flow.class);
 	}
 
@@ -82,7 +86,11 @@ public class FlowDeleterModule extends AnnotatedForegroundModule {
 		try {
 			transactionHandler = new TransactionHandler(dataSource);
 
-			List<Flow> flows = this.daoFactory.getFlowDAO().getAll(FLOW_QUERY, transactionHandler);
+			HighLevelQuery<Flow> query = new HighLevelQuery<>();
+			
+			query.setRelations(FLOW_RELATIONS);
+			
+			List<Flow> flows = this.daoFactory.getFlowDAO().getAll(query, transactionHandler);
 
 			if(flows != null){
 
@@ -158,6 +166,112 @@ public class FlowDeleterModule extends AnnotatedForegroundModule {
 		return new SimpleForegroundModuleResponse(getHTML("Flows deleted", CollectionUtils.getSize(deletedFlows) + " flows deleted, " + skippedCount + " flows skipped."));
 	}
 
+	@WebPublic
+	public synchronized ForegroundModuleResponse deleteFlowsByID(HttpServletRequest req, HttpServletResponse res, User user, URIParser uriParser) throws Throwable {
+
+		Integer submittedID = NumberUtils.toInt(req.getParameter("code"));
+		
+		List<Integer> flowIDs = NumberUtils.toInt(req.getParameterValues("id"));
+
+		if (submittedID == null || submittedID != randomNumber || flowIDs == null) {
+
+			return new SimpleForegroundModuleResponse(getHTML("Flow deleter (use with caution!)", "Submit the code " + randomNumber + " and id parameters to delete selected flows without flow instances."));
+		}
+
+		List<Flow> deletedFlows = null;
+		List<FlowFamily> deletedFlowFamilies = null;
+		
+		int skippedCount = 0;
+
+		log.info("User " + user + " deleting all flows without flow instances...");
+
+		TransactionHandler transactionHandler = null;
+
+		try {
+			transactionHandler = new TransactionHandler(dataSource);
+
+			HighLevelQuery<Flow> query = new HighLevelQuery<>();
+			
+			query.setRelations(FLOW_RELATIONS);
+			
+			query.addParameter(flowIDParamFactory.getWhereInParameter(flowIDs));
+			
+			List<Flow> flows = this.daoFactory.getFlowDAO().getAll(query, transactionHandler);
+
+			if(flows != null){
+
+				deletedFlows = new ArrayList<Flow>(flows.size());
+				deletedFlowFamilies = new ArrayList<FlowFamily>(flows.size()/2);
+				
+				for(Flow flow : flows){
+					
+					if(hasFlowInstances(flow)){
+						
+						log.info("Flow " + flow + " has one or more flow instances, skipping flow.");
+						
+						skippedCount++;
+						
+						continue;
+					}
+					
+					this.daoFactory.getFlowDAO().delete(flow, transactionHandler);
+
+					if (flow.getSteps() != null) {
+
+						for (Step step : flow.getSteps()) {
+
+							if (step.getQueryDescriptors() != null) {
+
+								for (QueryDescriptor queryDescriptor : step.getQueryDescriptors()) {
+
+									if (queryDescriptor.getEvaluatorDescriptors() != null) {
+
+										for (EvaluatorDescriptor evaluatorDescriptor : queryDescriptor.getEvaluatorDescriptors()) {
+
+											evaluationHandler.deleteEvaluator(evaluatorDescriptor, transactionHandler);
+										}
+									}
+
+									queryHandler.deleteQuery(queryDescriptor, transactionHandler);
+								}
+							}
+						}
+					}
+
+					deletedFlows.add(flow);
+					
+					//Check if the flow family has any more flow, else delete the family too
+					if (!hasFlows(flow.getFlowFamily(), transactionHandler)) {
+
+						this.daoFactory.getFlowFamilyDAO().delete(flow.getFlowFamily(), transactionHandler);
+						
+						deletedFlowFamilies.add(flow.getFlowFamily());
+					}
+				}
+			}
+			
+			transactionHandler.commit();
+			
+			if(!CollectionUtils.isEmpty(deletedFlows)){
+				
+				systemInterface.getEventHandler().sendEvent(Flow.class, new CRUDEvent<Flow>(Flow.class, CRUDAction.DELETE, deletedFlows), EventTarget.ALL);
+				
+				if(!CollectionUtils.isEmpty(deletedFlowFamilies)){
+					
+					systemInterface.getEventHandler().sendEvent(FlowFamily.class, new CRUDEvent<FlowFamily>(FlowFamily.class, CRUDAction.DELETE, deletedFlowFamilies), EventTarget.ALL);
+				}
+			}
+
+		} finally {
+
+			randomNumber = RandomUtils.getRandomInt(10000, 10000000);
+
+			TransactionHandler.autoClose(transactionHandler);
+		}
+
+		return new SimpleForegroundModuleResponse(getHTML("Flows deleted", CollectionUtils.getSize(deletedFlows) + " flows deleted, " + skippedCount + " flows skipped."));
+	}
+	
 	private boolean hasFlowInstances(Flow flow) throws SQLException {
 
 		HighLevelQuery<FlowInstance> query = new HighLevelQuery<FlowInstance>(); 
