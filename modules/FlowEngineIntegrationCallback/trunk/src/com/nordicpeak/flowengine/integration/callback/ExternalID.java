c11cbe7d7a4b21f3837aac3c0084c9b505b2aa3c
robertolofsson <robertolofsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2021-04-01 10:46:53 +0000

    - Added mutex based synchronization
    git-svn-id: svn://svn.sundsvall.se/oep-dev@1871 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- modules/FlowEngineIntegrationCallback/trunk/src/com/nordicpeak/flowengine/integration/callback/ExternalID.java
+++ modules/FlowEngineIntegrationCallback/trunk/src/com/nordicpeak/flowengine/integration/callback/ExternalID.java
@@ -87,4 +87,44 @@ public class ExternalID {
         this.system = value;
     }
 
+	@Override
+	public int hashCode() {
+
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((system == null) ? 0 : system.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		ExternalID other = (ExternalID) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (system == null) {
+			if (other.system != null) {
+				return false;
+			}
+		} else if (!system.equals(other.system)) {
+			return false;
+		}
+		return true;
+	}
+
 }
