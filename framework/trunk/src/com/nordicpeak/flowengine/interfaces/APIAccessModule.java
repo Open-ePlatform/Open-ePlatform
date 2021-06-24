84b35ff64c7b94b2fdf86cc4f4c220b6898c01a0
antonolsson <antonolsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2021-06-24 13:53:32 +0000

    - Move API Access Module to own repository
    git-svn-id: svn://svn.sundsvall.se/oep-dev@1958 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

---
+++ framework/trunk/src/com/nordicpeak/flowengine/interfaces/APIAccessModule.java
@@ -0,0 +1,20 @@
+package com.nordicpeak.flowengine.interfaces;
+
+import java.sql.SQLException;
+
+import se.unlogic.hierarchy.core.beans.User;
+import se.unlogic.hierarchy.core.exceptions.AccessDeniedException;
+
+import com.nordicpeak.flowengine.beans.Flow;
+import com.nordicpeak.flowengine.beans.FlowFamily;
+
+public interface APIAccessModule {
+
+	public boolean hasAccess(Integer familyID, User user) throws SQLException;
+	public boolean hasAccess(FlowFamily family, User user) throws SQLException;
+	public boolean hasAccess(Flow flow, User user) throws SQLException;
+		
+	public void accessCheck(Integer flowFamilyID, User user) throws SQLException, AccessDeniedException;
+	public void accessCheck(FlowFamily family, User user) throws SQLException, AccessDeniedException;
+	public void accessCheck(Flow flow, User user) throws SQLException, AccessDeniedException;
+}
---
+++ framework/trunk/src/com/nordicpeak/flowengine/utils/APIAccessUtils.java
@@ -0,0 +1,67 @@
+package com.nordicpeak.flowengine.utils;
+
+import java.sql.SQLException;
+
+import se.unlogic.hierarchy.core.beans.User;
+import se.unlogic.hierarchy.core.exceptions.AccessDeniedException;
+
+import com.nordicpeak.flowengine.beans.Flow;
+import com.nordicpeak.flowengine.beans.FlowFamily;
+import com.nordicpeak.flowengine.interfaces.APIAccessModule;
+
+public class APIAccessUtils {
+
+	public static boolean hasAccess(APIAccessModule apiAccessModule, Integer familyID, User user) throws SQLException {
+		
+		if (apiAccessModule != null) {
+			
+			return apiAccessModule.hasAccess(familyID, user);
+		}
+		
+		return true;
+	}
+
+	public static boolean hasAccess(APIAccessModule apiAccessModule, FlowFamily family, User user) throws SQLException {
+
+		if (apiAccessModule != null) {
+			
+			return apiAccessModule.hasAccess(family, user);
+		}
+		
+		return true;
+	}
+
+	public static boolean hasAccess(APIAccessModule apiAccessModule, Flow flow, User user) throws SQLException {
+
+		if (apiAccessModule != null) {
+			
+			return apiAccessModule.hasAccess(flow, user);
+		}
+		
+		return true;
+	}
+
+	public static void accessCheck(APIAccessModule apiAccessModule, Integer flowFamilyID, User user) throws SQLException, AccessDeniedException {
+
+		if (apiAccessModule != null) {
+			
+			apiAccessModule.accessCheck(flowFamilyID, user);
+		}
+	}
+
+	public static void accessCheck(APIAccessModule apiAccessModule, FlowFamily family, User user) throws SQLException, AccessDeniedException {
+
+		if (apiAccessModule != null) {
+			
+			apiAccessModule.accessCheck(family, user);
+		}
+	}
+
+	public static void accessCheck(APIAccessModule apiAccessModule, Flow flow, User user) throws SQLException, AccessDeniedException {
+
+		if (apiAccessModule != null) {
+			
+			apiAccessModule.accessCheck(flow, user);
+		}
+	}
+}
