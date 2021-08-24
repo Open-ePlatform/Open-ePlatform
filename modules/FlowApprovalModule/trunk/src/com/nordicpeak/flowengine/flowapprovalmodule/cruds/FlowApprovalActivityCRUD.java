cece82dd91b9822f7160bc5edde32b65b24f921d
mattiashallin <mattiashallin@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2021-08-24 11:20:08 +0000

    - Organized imports
    git-svn-id: svn://svn.sundsvall.se/oep-dev@1986 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- modules/FlowApprovalModule/trunk/src/com/nordicpeak/flowengine/flowapprovalmodule/cruds/FlowApprovalActivityCRUD.java
+++ modules/FlowApprovalModule/trunk/src/com/nordicpeak/flowengine/flowapprovalmodule/cruds/FlowApprovalActivityCRUD.java
@@ -8,7 +8,6 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.lucene.util.CollectionUtil;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
