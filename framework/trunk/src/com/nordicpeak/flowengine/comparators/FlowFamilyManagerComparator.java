ad20dcebe51aef2ac9c5748dfba2feccec84b158
robertolofsson <robertolofsson@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2022-01-13 09:24:25 +0000

    - Updated comparator to follow contract
    git-svn-id: svn://svn.sundsvall.se/oep-dev@2079 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- framework/trunk/src/com/nordicpeak/flowengine/comparators/FlowFamilyManagerComparator.java
+++ framework/trunk/src/com/nordicpeak/flowengine/comparators/FlowFamilyManagerComparator.java
@@ -7,42 +7,32 @@ import se.unlogic.hierarchy.foregroundmodules.usersessionadmin.UserNameComparato
 import com.nordicpeak.flowengine.beans.FlowFamilyManager;
 
 public class FlowFamilyManagerComparator implements Comparator<FlowFamilyManager> {
-	
+
 	private static final FlowFamilyManagerComparator INSTANCE = new FlowFamilyManagerComparator();
-	
+
 	public static FlowFamilyManagerComparator getInstance() {
+
 		return INSTANCE;
 	}
-	
+
 	@Override
 	public int compare(FlowFamilyManager o1, FlowFamilyManager o2) {
-		
-		boolean a1 = o1.isActive();
-		boolean a2 = o2.isActive();
-		
-		if (a1 == a2) {
+
+		if (o1.getUser() != null && o2.getUser() != null) {
+
+			return UserNameComparator.getInstance().compare(o1.getUser(), o2.getUser());
 			
-			if (o1.getUser() != null && o2.getUser() != null) {
-				
-				return UserNameComparator.getInstance().compare(o1.getUser(), o2.getUser());
-				
-			} else if (o1.getUser() != null) {
-				
-				return -1;
-				
-			} else {
-				
-				return 1;
-			}
-			
-		} else if (a1) {
-			
+		} else if (o1.getUser() != null) {
+
 			return -1;
-			
-		} else {
-			
+
+		} else if(o1.getUser() != null){
+
 			return 1;
 		}
+		
+		return 0;
+		
 	}
-	
+
 }
