b21a8c5865a72a0997eb3ddbb59fa34082555fd2
fredrikfrost <fredrikfrost@fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18>
2022-04-07 13:51:25 +0000

    Set FlowFamilyID as searchable.
    git-svn-id: svn://svn.sundsvall.se/oep-dev@2179 fcb98548-a2c4-3c4b-99c6-acc7aeb3bb18

--- framework/trunk/src/com/nordicpeak/flowengine/staticcontent/js/flowadminmodule.js
+++ framework/trunk/src/com/nordicpeak/flowengine/staticcontent/js/flowadminmodule.js
@@ -76,7 +76,7 @@ $(function() {
 			{ name:"submittedInstances", searchable: false, visible: (userColumnVisible.submittedInstances != null ? userColumnVisible.submittedInstances=='true' : true) && !FlowAdmin.hideSubmittedInstances },
 			{ name:"notSubmittedInstances", searchable: false, visible: (userColumnVisible.notSubmittedInstances != null ? userColumnVisible.notSubmittedInstances=='true' : true) && !FlowAdmin.hideNotSubmittedInstances },
 			{ name:"flowFamilyLastReviewed", type: "date", visible: (userColumnVisible.flowFamilyLastReviewed != null ? userColumnVisible.flowFamilyLastReviewed=='true' : true) && !FlowAdmin.hideFlowFamilyLastReviewed },
-			{ name:"flowFamilyID", searchable: false, visible: (userColumnVisible.flowFamilyID != null ? userColumnVisible.flowFamilyID=='true' : false)},
+			{ name:"flowFamilyID", searchable: true, visible: (userColumnVisible.flowFamilyID != null ? userColumnVisible.flowFamilyID=='true' : false)},
 			{ name:"organization", searchable: false, visible: (userColumnVisible.organization != null ? userColumnVisible.organization=='true' : false) && !FlowAdmin.hideOrganization }
 		]
 		
